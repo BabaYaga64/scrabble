8dbab25177bd4c1c794604aeb2e4261266d7fe68
Brian Kropff & Bojana Skarich <student@epicodus.com>
2015-03-12 10:49:18 -0700

    Add first test that assigns value to single letter, lowest value.

---
+++ tests/LetterValueTest.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once "src/LetterValue.php"
+
+    class LetterValueTest extends PHPUnit_Framework_TestCase
+    {
+        function test_SimpleValueAssignment()
+        {
+
+            //Arrange
+            $test_LetterValue = new LetterValue;
+            $input = "a"
+
+            //Act
+            $result = $test_LetterValue->assignLetterValue($input)
+
+            //Assert
+            $this->assertEquals(1, $result);
+
+        }
+    }
+
+
+
+?>
